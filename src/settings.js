30811281ce4b57fa26bce7444212e4bd65bde44c
龙汪洋 <13384618+longwangyang@user.noreply.gitee.com>
2024-08-01 23:22:43 +0800

    first

---
+++ src/settings.js
@@ -0,0 +1,16 @@
+module.exports = {
+  title: '星辰后台管理系统', // 网站的标题
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  fixedHeader: true, // 是否固定头部
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  sidebarLogo: true
+  // 是否显示侧边栏logo
+}
