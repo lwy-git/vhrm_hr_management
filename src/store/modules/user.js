7bfd3cd8ca486f5fbd7830aaeb3c4b69b93fe5af
龙汪洋 <13384618+longwangyang@user.noreply.gitee.com>
2024-08-17 19:12:33 +0800

    【feat]vue-cli解决跨域问题

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,97 +1,28 @@
-import { login, logout, getInfo } from '@/api/user'
 import { getToken, setToken, removeToken } from '@/utils/auth'
-import { resetRouter } from '@/router'
-
-const getDefaultState = () => {
-  return {
-    token: getToken(),
-    name: '',
-    avatar: ''
-  }
+const state = {
+  token: getToken()// 从缓存读取token初始值
 }
-
-const state = getDefaultState()
-
 const mutations = {
-  RESET_STATE: (state) => {
-    Object.assign(state, getDefaultState())
-  },
-  SET_TOKEN: (state, token) => {
+  setToken(state, token) {
     state.token = token
+    setToken(token)// 获取的token同步到缓存
   },
-  SET_NAME: (state, name) => {
-    state.name = name
-  },
-  SET_AVATAR: (state, avatar) => {
-    state.avatar = avatar
+  removeToken() {
+    state.token = null// 删除vuex中的token
+    removeToken()// 退出时从缓存删除token
   }
 }
-
 const actions = {
-  // user login
-  login({ commit }, userInfo) {
-    const { username, password } = userInfo
-    return new Promise((resolve, reject) => {
-      login({ username: username.trim(), password: password }).then(response => {
-        const { data } = response
-        commit('SET_TOKEN', data.token)
-        setToken(data.token)
-        resolve()
-      }).catch(error => {
-        reject(error)
-      })
-    })
-  },
-
-  // get user info
-  getInfo({ commit, state }) {
-    return new Promise((resolve, reject) => {
-      getInfo(state.token).then(response => {
-        const { data } = response
-
-        if (!data) {
-          return reject('Verification failed, please Login again.')
-        }
-
-        const { name, avatar } = data
-
-        commit('SET_NAME', name)
-        commit('SET_AVATAR', avatar)
-        resolve(data)
-      }).catch(error => {
-        reject(error)
-      })
-    })
-  },
-
-  // user logout
-  logout({ commit, state }) {
-    return new Promise((resolve, reject) => {
-      logout(state.token).then(() => {
-        removeToken() // must remove  token  first
-        resetRouter()
-        commit('RESET_STATE')
-        resolve()
-      }).catch(error => {
-        reject(error)
-      })
-    })
-  },
-
-  // remove token
-  resetToken({ commit }) {
-    return new Promise(resolve => {
-      removeToken() // must remove  token  first
-      commit('RESET_STATE')
-      resolve()
-    })
+  login(context, data) {
+    console.log(data)
+    // todo 调用接口返回token
+    context.commit('setToken', '123456')
   }
-}
 
+}
 export default {
   namespaced: true,
   state,
   mutations,
   actions
 }
-
--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,137 +1,50 @@
 <template>
-  <!-- 关键声明： id  和 width 和  height 都会影响图表的展示-->
   <div>
-    <div
-      id="demo"
-      style="width: 500px;height:400px;"
-    />
-    <button
-      @click="changeType"
-    >xxx</button>
-  </div></template>
+    <div class="box">
+      <div class="a">
+        <button
+          style="position: absolute;
+  margin: 10px 10px ;"
+        >xxx</button>
+      </div>
+      <div class="b">
+        <button
+          style="position: absolute;
+  margin: 10px 10px;"
+        >xxx</button>
+      </div>
+    </div>
+  </div>
+</template>
 
 <script>
-
 export default {
-  name: 'HelloWord',
-  data() {
-    return {
-      xData1: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'],
-      yData1: [140, 150, 80, 200, 400, 500],
-      yData2: [180, 250, 10, 300, 600, 200],
-      type: 'line',
-      isshow: true,
-      radius: [],
-      center: []
-    }
-  },
-  mounted() {
-    // 进入页面就执行一次
-    this.changeType()
-    this.drawChart()
-  },
-  methods: {
-    changeType() {
-      if (this.type === 'line') {
-        this.type = 'pie'
-        this.radius = ['40%', '70%']
-        this.center = ['50%', '70%']
-      } else if (this.type === 'pie') {
-        this.type = 'line'
-      }
-
-      this.isshow = !this.isshow
-      console.log(this.type)
-      this.drawChart()
-    },
-    drawChart() {
-      // 2. 基于准备好的dom，初始化echarts实例
-      // 此处的意思就是，对 demo 元素 进行图表初始化的相关操作
-      var myChart = this.$echarts.init(document.getElementById('demo'))
-      // 3. 指定图表的配置项和数据
-      // 该处就是图表内容，在官网的示例里面，要复制过来到项目里面的也是这一块内容
-      var option = {
-        tooltip: {
-          trigger: 'item'
-        },
-        legend: {
-          data: ['数据1', '数据2']
-        },
-        // toolbox: {
-        //   show: true,
-        //   feature: {
-        //     magicType: { show: true, type: ['pie', 'line'], title: { line: '切换为折线图', pie: '切换为柱状图图' }}
-        //   }
-        // },
-        calculable: true,
-        xAxis: {
-          type: 'category',
-          boundaryGap: false,
-          data: this.xData1,
-          show: this.isshow // 隐藏 X 轴
-
-        },
-        yAxis: [
-          {
-            type: 'value'
-          }
-        ],
-        series: [
-          {
-            name: '数据1',
-            type: this.type, // 形状为折线图
-            data: this.yData1,
-            itemStyle: {
-              color: '#5087EC'
-            },
-            label: {
-              // 柱状图上方文本标签，默认展示数值信息
-              show: true,
-              position: 'top'
-            },
-            radius: this.radius,
-            center: this.center,
-            startAngle: 180,
-            endAngle: 360
-          },
-          {
-            name: '数据2',
-            type: this.type, // 形状为柱状图
-            data: this.yData2,
-            radius: this.radius,
-            center: this.center,
-            startAngle: 180,
-            endAngle: 360,
-            itemStyle: {
-              colors: ['#68BBC4', 'red', 'blue']
-            },
-            label: {
-              // 柱状图上方文本标签，默认展示数值信息
-              show: true,
-              position: 'top'
-            }
-          }
-        ]
-      }
-      // 4.使用刚指定的配置项和数据显示图表。
-      myChart.setOption(option)
-      // myChart.on('legendselectchanged', function(params) {
-      //   console.log('图例项被点击:', params)
-
-      //   // 在这里可以根据选中的图例项执行自定义逻辑
-      //   if (params.name === '数据1') {
-      //     // alert('你点击了数据1的图例项')
-      //     this.changeType()
-      //   } else if (params.name === '数据2') {
-      //     alert('你点击了数据2的图例项')
-      //   }
-      // })
-    }
-  }
-
+  name: 'HelloWord'
 }
 </script>
 
 <style scoped>
-
+.box {
+  display: flex;
+  justify-content: space-between;
+  position: relative;
+}
+.a {
+  display: flex;
+  justify-content: flex-end;
+  position: relative;
+  height: 300px;
+  width: 100%;
+  margin-right: 10px;
+  background-color: gold;
+}
+.b {
+  display: flex;
+  justify-content: flex-end;
+  position: relative;
+  height: 300px;
+  margin-right: 10px;
+  width: 100%;
+  background-color: pink;
+}
 </style>
